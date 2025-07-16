71005be58b6f22034fcbb9a7364ccf786fae3557
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2025-07-16 23:17:35 +0530

    changes to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  server:{
+    proxy:{
+      "/api":{
+        target:'https://wedease-server.onrender.com/api',
+        changeOrigin:true,
+        secure:true
+      }
+    }
+  }
 })
